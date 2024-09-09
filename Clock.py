c07c09818974983085320cf70a7fd2bb8e68580c
Mikey Dowsett <mikeydowsett@proton.me>
2024-09-08 21:09:40 -0500

    Random pictures, start of alarm functionality

--- Clock.py
+++ Clock.py
@@ -1,9 +1,13 @@
-﻿import tkinter
+﻿import random
+import tkinter
+import winsound
 from tkinter import *
 from tkinter.ttk import *
 import time
 import requests, json
 from PIL import Image, ImageTk
+from playsound import playsound
+import threading
 
 font = "Nunito"
 
@@ -13,8 +17,10 @@ class Clock(Tk):
 
         self.columnconfigure(0, weight=1)
         self.attributes('-fullscreen', True)
+        #threading.Thread(target=playsound, args=("Sounds/0.wav",True), daemon=True).start()
+
         self.screen_height = self.winfo_screenheight()
-        self.smallest_text = int(self.screen_height * 0.05)
+        self.small_text = int(self.screen_height * 0.05)
         self.medium_text = int(self.screen_height * 0.07)
         self.large_text = int(self.screen_height * 0.08)
         self.small_image = int(self.screen_height * 0.15)
@@ -22,12 +28,15 @@ class Clock(Tk):
 
         # Display the time
         self.clock_frame = Frame()
-        self.date_label = Label(self.clock_frame, text="05/25/2024", font=(font, self.large_text))
+        self.date_label = Label(self.clock_frame, text="05/25/2024", font=(font, self.small_text))
         self.date_label.pack(anchor='e')
         self.clock_label = Label(self.clock_frame, text="00:00:00", font=(font, self.large_text))
         self.clock_label.pack(anchor='e')
-        self.clock_frame.grid(row=0, column=1, pady=20, padx=20, sticky='nesw')
+        self.clock_frame.grid(row=0, rowspan=3, column=1, pady=20, padx=(0, 20), sticky='ns')
+        self.cozy_image_label = Label(self.clock_frame)
+        self.cozy_image_label.pack()
         self.update_clock()
+        self.pick_cozy_image()
 
         # Get and Display current weather
         self.api_key_weather = ""
@@ -37,7 +46,7 @@ class Clock(Tk):
         #Create all the weather components
         self.weather_frame = Frame()
         self.current_temp_label = Label(self.weather_frame, text="25", font=(font, self.medium_text))
-        self.feels_like_temp_label = Label(self.weather_frame, text="25", font=(font, int(self.smallest_text/2)))
+        self.feels_like_temp_label = Label(self.weather_frame, text="25", font=(font, int(self.small_text / 2)))
         self.weather_image_label = Label(self.weather_frame)
         self.current_weather_label = Label(self.weather_frame, text="Thunderstorm", font=(font, self.large_text))
 
@@ -52,10 +61,10 @@ class Clock(Tk):
         self.high_low_frame = Frame()
         self.temp_low_image = ImageTk.PhotoImage(Image.open("Images/lowtemp.png").resize((self.small_image, self.small_image)))
         self.temp_low_image_label = Label(self.high_low_frame, image=self.temp_low_image)
-        self.temp_low_label = Label(self.high_low_frame, text="20", font=(font, self.smallest_text))
+        self.temp_low_label = Label(self.high_low_frame, text="20", font=(font, self.small_text))
         self.temp_high_image = ImageTk.PhotoImage(Image.open("Images/hightemp.png").resize((self.small_image, self.small_image)))
         self.temp_high_image_label = Label(self.high_low_frame, image=self.temp_high_image)
-        self.temp_high_label = Label(self.high_low_frame, text="25", font=(font, self.smallest_text))
+        self.temp_high_label = Label(self.high_low_frame, text="25", font=(font, self.small_text))
 
         self.temp_low_image_label.grid(row=0, column=0)
         self.temp_low_label.grid(row=0, column=1)
@@ -69,7 +78,7 @@ class Clock(Tk):
         self.humidity_frame = Frame(self.weather_data_frame)
         self.humidity_image = ImageTk.PhotoImage(Image.open("Images/humidity.png").resize((self.small_image, self.small_image)))
         self.humidity_image_label = Label(self.humidity_frame, image=self.humidity_image)
-        self.humidity_label = Label(self.humidity_frame, text="15", font=(font, self.smallest_text))
+        self.humidity_label = Label(self.humidity_frame, text="15", font=(font, self.small_text))
         self.humidity_image_label.grid(row=0, column=0, sticky='n')
         self.humidity_label.grid(row=0, column=1)
         self.humidity_frame.grid(row=0, column=0, pady=10, padx=10, sticky='nw')
@@ -78,7 +87,7 @@ class Clock(Tk):
         self.uv_frame = Frame(self.weather_data_frame)
         self.uv_image = ImageTk.PhotoImage(Image.open("Images/uv.png").resize((self.small_image, self.small_image)))
         self.uv_image_label = Label(self.uv_frame, image=self.uv_image)
-        self.uv_label = Label(self.uv_frame, text="15", font=(font, self.smallest_text))
+        self.uv_label = Label(self.uv_frame, text="15", font=(font, self.small_text))
         self.uv_image_label.grid(row=0, column=0, sticky='n')
         self.uv_label.grid(row=0, column=1)
         self.uv_frame.grid(row=0, column=1, pady=10, padx=10, sticky='nw')
@@ -95,8 +104,9 @@ class Clock(Tk):
         self.clock_label.config(text=f'{hours}:{minutes}:{seconds}')
         self.date_label.config(text=f'{time.strftime("%d %B")}')
         self.after(1000, lambda:self.update_clock())
-        if int(minutes) % 30 == 0 and int(seconds) == 0:
+        if int(minutes) % 10 == 0 and int(seconds) == 0:
             self.update_weather()
+            self.pick_cozy_image()
 
     def update_weather(self):
         print("Weather Updated")
@@ -122,6 +132,11 @@ class Clock(Tk):
             self.weather_image_label.config(image=self.weather_image)
 
             self.current_weather_label.config(text=weather_data_weather["main"])
+
+    def pick_cozy_image(self):
+        self.cozy_image = Image.open(f"Cozy/{random.randrange(0, 9, 1)}.png")
+        self.cozy_image = ImageTk.PhotoImage(self.cozy_image.resize((self.large_image, self.large_image)))
+        self.cozy_image_label.config(image=self.cozy_image)
 
 if __name__ == "__main__":
     window = Clock()
